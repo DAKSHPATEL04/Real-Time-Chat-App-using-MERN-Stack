0ec9e10294aa172a7450975b17b0a68eb558c281
Daksh Patel <pateldaksh382@gmail.com>
2024-05-29 10:59:26 +0530

    b

--- frontend/src/Pages/Chatpage.jsx
+++ frontend/src/Pages/Chatpage.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import { ChatState } from "../../Context/ChatProvider";
 import { Box } from "@chakra-ui/react";

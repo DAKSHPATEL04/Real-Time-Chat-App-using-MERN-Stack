d10e938bb2e1c3e518d45c73ccfa38664ca0f1d2
Daksh Patel <pateldaksh382@gmail.com>
2024-06-04 10:44:59 +0530

    v

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,16 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
   optimizeDeps: {
     exclude: ['socket.io-client']
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
+      '/api': 'http://localhost:5000',
     },
   },
   build: {
     outDir: 'dist',
   },
-});
+};
